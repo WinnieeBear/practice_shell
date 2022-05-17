8ab68bdf5ab781be2dcce38e4510759bf735e97d
WinnieeBear <gcf1129@gmail.com>
2022-05-17 23:05:50 +0800

    finish chap9 random number

---
+++ chap9/example9-16.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+MAXCOUNT=25
+SEED=
+
+random_numbers() {
+    local count=0
+    local number
+
+    while [ "$count" -lt "$MAXCOUNT" ]; do
+        number=$RANDOM
+        echo -n "$number "
+        let "count++"
+    done
+}
+
+echo; echo
+
+
+SEED=1
+RANDOM=$SEED
+echo "Random seed = $SEED"
+random_numbers
+
+echo; echo
+
+RANDOM=$SEED
+echo; echo "Again, with the same random seed"
+echo "Random seed = $SEED"
+random_numbers
+
+echo; echo
+
+SEED=2
+RANDOM=$SEED
+echo "Random seed = $SEED"
+random_numbers
+
+echo; echo
+
+SEED=$(head -1 /dev/urandom | od -N 1 | awk '{ print $2 }' | sed s/^0*//)
+RANDOM=$SEED
+echo "Random seed = $SEED"
+random_numbers
+
+echo; echo
+
+exit 0
